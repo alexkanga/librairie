91f8282c6d7dae163308522ff1809cca3a7081ee
alexkanga <kalexane@gmail.com>
2023-10-27 16:20:52 +0000

    add port env

--- Backend/server.js
+++ Backend/server.js
@@ -7,17 +7,7 @@ const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   
-  if (req.url === "/" && req.method === "GET") {
-    fs.readFile("../indo.html", "utf-8", (err, data) => {
-      if (!err) {
-        res.setHeader("content-type", "text/html");
-        res.end(data);
-      }
-    });
-    // on revoie les utilisateur apres les avoir transformés en chaine de caractère
-  }
-
-  else if (req.url === "/auth/register" && req.method === "POST") {
+  if (req.url === "/auth/register" && req.method === "POST") {
     let body = "";
     req.on("data", (data) => {     
       body += data;
